3c0e7b83bb6945c1eabf9e3b5869a82034a028de
Vanessa Sun <vsun@connectdotz.com>
2013-09-23 17:07:08 -0400

    adjust a few settngs
    adjust a few settngs

--- titan/recipes/setup.rb
+++ titan/recipes/setup.rb
@@ -40,50 +40,62 @@ bash "install_titan" do
   user "root"
   code <<-EOH
 
-	echo "base_dir=#{base_dir}"
-	echo "log_dir=#{log_dir}"
-	echo "rexster_home=#{rexster_home}"
-	echo "titan_home=#{titan_home}"
+    echo "base_dir=#{base_dir}"
+    echo "log_dir=#{log_dir}"
+    echo "rexster_home=#{rexster_home}"
+    echo "titan_home=#{titan_home}"
 
-	#install ttx home  directory
-	mkdir -p #{base_dir}
-	cd #{base_dir}
+    #install ttx home  directory
+    mkdir -p #{base_dir}
+    cd #{base_dir}
 
-	###### download titan components
-	echo "installing #{node['titan']['rexster_path']} ..."
- 	zip_file="rexster-server-#{node['titan']['rexster_version']}"
-	curl -# -L -k "http://tinkerpop.com/downloads/rexster/$zip_file.zip" > $zip_file.zip
-	unzip $zip_file.zip 
-	ln -s $zip_file #{node['titan']['rexster_path']}
-	rm $zip_file.zip
+    ###### download titan components
+    echo "installing #{node['titan']['rexster_path']} ..."
+    zip_file="rexster-server-#{node['titan']['rexster_version']}"
+    if [ -d "#{node['titan']['rexster_path']" ]; then
+        curl -# -L -k "http://tinkerpop.com/downloads/rexster/$zip_file.zip" > $zip_file.zip
+        unzip $zip_file.zip 
+        ln -s $zip_file #{node['titan']['rexster_path']}
+        rm $zip_file.zip
+    else
+        echo "directory existed, skipped"
+    fi
 
-	echo "installing rexster-console ..."
- 	zip_file2="rexster-console-#{node['titan']['rexster_version']}"
-	curl -# -L -k "http://tinkerpop.com/downloads/rexster/$zip_file2.zip" > $zip_file2.zip
-	unzip $zip_file2.zip 
-	ln -s $zip_file2 rexster-console
-	rm $zip_file2.zip
+    echo "installing rexster-console ..."
+    zip_file2="rexster-console-#{node['titan']['rexster_version']}"
+    if [ -d "rexster-console" ]; then
+    	curl -# -L -k "http://tinkerpop.com/downloads/rexster/$zip_file2.zip" > $zip_file2.zip
+    	unzip $zip_file2.zip 
+    	ln -s $zip_file2 rexster-console
+    	rm $zip_file2.zip
+    else
+        echo "directory existed, skipped"
+    fi
 
-	echo "installing titan-hbase ..."
- 	zip_file3="titan-hbase-#{node['titan']['titan_version']}"
-	curl -# -L -k "http://s3.thinkaurelius.com/downloads/titan/$zip_file3.zip" > $zip_file3.zip
-	unzip $zip_file3.zip 
-	ln -s $zip_file3 #{node['titan']['titan_path']}
-	rm $zip_file3.zip
+    echo "installing titan-hbase ..."
+    zip_file3="titan-hbase-#{node['titan']['titan_version']}"
+    if [ -d "#{node['titan']['titan_path']" ]; then
+    	curl -# -L -k "http://s3.thinkaurelius.com/downloads/titan/$zip_file3.zip" > $zip_file3.zip
+    	unzip $zip_file3.zip 
+    	ln -s $zip_file3 #{node['titan']['titan_path']}
+    	rm $zip_file3.zip
+    else
+        echo "directory existed, skipped"
+    fi
 
-	#install titan with rexster
-	ln -s #{titan_home}/lib #{rexster_home}/ext/titan
-	
-	#fix rexster/titan lucent library conflict
-	mkdir #{rexster_home}/titan_conflict
-	mv #{rexster_home}/lib/lucene* #{rexster_home}/titan_conflict
-	
-	#tar -xvopf /tmp/ttx-titan-dist.tar.gz
-	#rm -rf /tmp/ttx-titan-dist.tar.gz
+    #install titan with rexster
+    ln -s #{titan_home}/lib #{rexster_home}/ext/titan
+    
+    #fix rexster/titan lucent library conflict
+    mkdir #{rexster_home}/titan_conflict
+    mv #{rexster_home}/lib/lucene* #{rexster_home}/titan_conflict
+    
+    #tar -xvopf /tmp/ttx-titan-dist.tar.gz
+    #rm -rf /tmp/ttx-titan-dist.tar.gz
 
-	####### initialize log directory
-	mkdir -p #{log_dir}
-	chmod og+r #{log_dir}
+    ####### initialize log directory
+    mkdir -p #{log_dir}
+    chmod og+r #{log_dir}
   EOH
   # notifies :run, "bash[setup_rexster_titan]", :immediately
   notifies :create, "template[rexster_xml]", :immediately
