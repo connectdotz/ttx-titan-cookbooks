ef7882f4abbf99bfd9db832b791d09c3f681d2ba
Vanessa Sun <vsun@connectdotz.com>
2013-09-26 01:24:27 -0400

    fix metadata

--- ttx_es/metadata.rb
+++ ttx_es/metadata.rb
@@ -4,7 +4,7 @@ maintainer "thingtrix"
 maintainer_email "thingtrix@thingtrix.com"
 license "Apache"
 description "wrapper cookbook for installing elasticsearch"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+#long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 version "0.1"
 
 depends 'elasticsearch'
