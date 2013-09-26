b92fecfbbbcb2ed246755a8340e36a9298181b1c
Vanessa Sun <vsun@connectdotz.com>
2013-09-26 00:06:34 -0400

    added metadata for ttx_es

---
+++ ttx_es/metadata.rb
@@ -0,0 +1,10 @@
+name "ttx_es"
+
+maintainer "thingtrix"
+maintainer_email "thingtrix@thingtrix.com"
+license "Apache"
+description "wrapper cookbook for installing elasticsearch"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
+version "0.1"
+
+depends 'elasticsearch'
