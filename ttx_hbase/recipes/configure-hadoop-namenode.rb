085179c4728b83aa1d1674ebc90d773f87430210
Vanessa Sun <vsun@connectdotz.com>
2013-10-02 16:41:11 -0400

    added configure-hadoop-namenode

---
+++ ttx_hbase/recipes/configure-hadoop-namenode.rb
@@ -0,0 +1,20 @@
+#
+# Copyright 2013 ConnectDotz, LLC. All Rights Reserved.
+#
+
+#
+# triggered by the OpsWork configuration lifecycle event when any instance came online in the stack
+#
+
+include_recipe 'ttx_hbase::opsworks-context'
+include_recipe 'ttx_hbase::setup-hadoop-conf'
+include_recipe 'ttx_hbase::hadoop-services'
+
+execute "restart-hadoop-namenode" do
+	command "echo perform configure operation"
+	only_if { node[:ttx_hbase][:opsworks][:hadoop_fs_name_changed] == true}
+	notifies :run, "bash[setup-hadoop-conf]", :immediately
+	notifies :restart, "service[hadoop-namenode]", :delayed
+end
+
+
