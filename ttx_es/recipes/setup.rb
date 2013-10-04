06100012c7abb2dd2bd491932f1ce336fa743dc2
Vanessa Sun <vsun@connectdotz.com>
2013-10-04 16:46:28 -0400

    enable aws

--- ttx_es/recipes/setup.rb
+++ ttx_es/recipes/setup.rb
@@ -2,7 +2,7 @@ include_recipe 'elasticsearch::default'
 include_recipe 'elasticsearch::plugins'
 #include_recipe 'elasticsearch::ebs'
 #include_recipe 'elasticsearch::data'
-#include_recipe 'elasticsearch::aws'
+include_recipe 'elasticsearch::aws'
 #include_recipe 'elasticsearch::nginx'
 #include_recipe 'elasticsearch::proxy'
 
